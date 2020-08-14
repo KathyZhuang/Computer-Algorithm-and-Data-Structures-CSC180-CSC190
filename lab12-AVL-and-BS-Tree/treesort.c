8d9925c99beaab8b2d9163614d8e8e4269395d50
KathyZhuang <35270359+KathyZhuang@users.noreply.github.com>
2020-08-14 18:01:30 -0400

    Create treesort.c

---
+++ lab12-AVL-and-BS-Tree/treesort.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct bstNode {
+   int val;
+   struct bstNode *l;
+   struct bstNode *r;
+};
+typedef struct bstNode bstNode;
+
+struct avlNode {
+   int balance; /* -1 Left, 0 balanced, +1 Right */
+   int val;
+   struct avlNode *l;
+   struct avlNode *r;
+};
+typedef struct avlNode avlNode;
+
+struct qNode {
+   avlNode *pval;
+   struct qNode *nxt;
+};
+typedef struct qNode qNode;
+
+int add_bst(bstNode **root,int val) {
+   if (root == NULL) { return -1; }
+   if (*root == NULL) {
+      *root = (bstNode *)malloc(sizeof(bstNode));
+      (*root)->val = val;
+   } else {
+      if ((*root)->val > val){
+         add_bst(&((*root)->l),val);
+      }
+      else{
+         add_bst(&((*root)->r),val);
+      }
+   }
+}
+
+int printTreeInOrder(bstNode *root){
+   if (root == NULL){
+      return -1;
+   }
+   if (root->l != NULL){
+      printTreeInOrder(root->l);
+   }
+   printf("%d\n",(root->val));
+   if (root->r != NULL){
+      printTreeInOrder(root->r);
+   }
+}
+
+int getLength(bstNode *root){
+   int lh = 0;
+   int rh = 0;
+   if (root == NULL){
+      return 0;
+   }
+   lh = getLength(root->l);
+   rh = getLength(root->r);
+   if (lh > rh){
+      return (lh + 1);
+   }
+   else{
+      return (rh + 1);
+   }
+}
+
+int printLevelOrder(bstNode *root){
+   int i = 0;
+   int length = 0;
+   if (root == NULL){
+      return -1;
+   }
+   length = getLength(root);
+   for (i = 0; i < length; i++){
+      plo_helper(root,i);
+   }
+   return 0;
+}
+
+int plo_helper(bstNode *root, int level){
+   if (root == NULL){
+      return -1;
+   }
+   if (level == 0){
+      printf("%d",root->val);
+   }
+   else{
+      plo_helper(root->l, level - 1);
+      plo_helper(root->r, level - 1);
+   }
+}
+
+
+
+int main(void) {
+   int value = 0;
+   bstNode *root = NULL;
+   while (scanf("%d",&value) != EOF) {
+      add_bst(&root,value);
+   }
+   printTreeInOrder(root);
+   printLevelOrder(root);
+   return 0;
+}
+
+
